3f2b4bdf24dbedc364e7d36f275e1d206e4fd97d
Ashfaq Afzal Chowdhury <aacfahim@gmail.com>
2021-03-08 20:17:48 +0600

    Updates

---
+++ precache-manifest.2a981907c548ee921d95d82ea67d70a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca63e9a7ff9d22198e9c4e4196fb8985",
+    "url": "/portfolio/static/media/profile.ca63e9a7.jpg"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/portfolio/static/media/ab-img.8d7ae5ac.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "112167e76578ec54ad26",
+    "url": "/portfolio/static/js/main.112167e7.chunk.js"
+  },
+  {
+    "revision": "94848333bb267c5aae18",
+    "url": "/portfolio/static/js/1.94848333.chunk.js"
+  },
+  {
+    "revision": "112167e76578ec54ad26",
+    "url": "/portfolio/static/css/main.e4b37cc8.chunk.css"
+  },
+  {
+    "revision": "94848333bb267c5aae18",
+    "url": "/portfolio/static/css/1.98d735a2.chunk.css"
+  },
+  {
+    "revision": "f5d11784e491779f6aa3c13199c69ecd",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/react-deploy/precache-manifest.51b69de32458fc434449a568593f9a96.js"
+  "/portfolio/precache-manifest.2a981907c548ee921d95d82ea67d70a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/react-deploy/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
